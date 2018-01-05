4d798c4ca718509751ec583762c223f8068c4feb
fengxi <fengxi1986@gmail.com>
2018-01-05 19:07:11 +0800

    Add a decorator example

---
+++ python_exam/decorator/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+import functools
+
+def metric(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kw):
+        begin_time = time.time()
+        f = fn(*args, **kw)
+        end_time = time.time()
+        print '%s costs %s' % (fn.__name__, end_time - begin_time)
+        return f
+    return wrapper
+
+@metric
+def add1(x,  y):
+    time.sleep(0.1)
+    return x + y
+
+@metric
+def add2(x, y, z):
+    time.sleep(1)
+    return x + y + z
+
+f = add1(11, 22)
+g = add2(11, 22, 33)
