5c5a640b5daf5fd9ba3ed96bd5a0d05092ec897a
fengxi <fengxi1986@gmail.com>
2018-03-24 10:51:20 +0800

    adjust the folder structure, move cpp_exam, python_exam and sort to algorithm folder

---
+++ algorithm/array/build/CMakeFiles/test_linklist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_linklist.dir/src/array.cpp.o"
+  "CMakeFiles/test_linklist.dir/src/main.cpp.o"
+  "test_linklist.pdb"
+  "test_linklist"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_linklist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
